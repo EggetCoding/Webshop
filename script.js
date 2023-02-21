e0a38359cc6c11061915b9e4c4afbe99bdf0051e
EggetCoding <edward.lunden@elev.ga.ntig.se>
2023-02-21 14:28:42 +0100

    Update #5

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-let idForProduct = 0;
-
-function changeBg(){
-    var navbar = document.getElementById("navbar");
-    var navbarText = document.getElementById("navbarText");
-    var scrollValue = window.scrollY;
-
-
-    if(scrollValue < 450)
-    {
-        navbar.classList.remove("bgColor1");
-        navbar.classList.add("bgColor2");
-
-        navbarText.classList.remove("navbarTextColor1");
-        navbarText.classList.add("navbarTextColor2");
-    }
-    else
-    {
-        navbar.classList.remove("bgColor2");
-        navbar.classList.add("bgColor1");
-
-        navbarText.classList.remove("navbarTextColor2");
-        navbarText.classList.add("navbarTextColor1");
-    }
-}
-
-window.addEventListener("scroll", changeBg);
-
-
-
 window.onload = getParameter();
 
 function getParameter(parameter){
